128fd74badeaae69a025fb78711fa93bf75aa409
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-11-26 17:18:12 -0300

    feat: create model exercise

---
+++ src/app/models/Exercise.js
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Exercise extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        url: Sequelize.STRING,
+        min_age: Sequelize.INTEGER,
+        max_age: Sequelize.INTEGER,
+        subject: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+export default Exercise;
